6e9b75371497dd1712890a19f2e43e49ad560680
andrea leo <andrea.leo83@gmail.com>
2018-11-20 17:54:09 +0100

    gcs fix

--- functions/chat-thumbnail.js
+++ functions/chat-thumbnail.js
@@ -17,7 +17,9 @@
 
 // [START import]
 const functions = require('firebase-functions');
-const gcs = require('@google-cloud/storage')();
+const admin = require('firebase-admin');
+// const gcs = require('@google-cloud/storage');
+// do not use this. const gcs = require('@google-cloud/storage')();
 const spawn = require('child-process-promise').spawn;
 const path = require('path');
 const os = require('os');
@@ -57,7 +59,10 @@ exports.generateThumbnail = functions.storage.object().onFinalize((object) => {
 
   // [START thumbnailGeneration]
   // Download file from bucket.
-  const bucket = gcs.bucket(fileBucket);
+
+  // const bucket = gcs.bucket(fileBucket);
+  const bucket = admin.storage().bucket(fileBucket);
+
   const tempFilePath = path.join(os.tmpdir(), fileName);
   const metadata = {
     contentType: contentType,
