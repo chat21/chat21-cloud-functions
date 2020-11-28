9b26dbdb7f061887ca91afd80f86b87cc314b844
andrea leo <andrea.leo83@gmail.com>
2020-11-28 11:20:52 +0100

    default restored to usa

--- functions/config.js
+++ functions/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    //region:'us-central1',     //Find other locations here https://firebase.google.com/docs/functions/locations
-    region: 'europe-west1',
+    region:'us-central1',     //Find other locations here https://firebase.google.com/docs/functions/locations
+    //region: 'europe-west1',
   };
